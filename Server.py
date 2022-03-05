5d723b6bba70460345f96f7032d288475b86fcb2
anitanadvikova <39883551+anitanadvikova@users.noreply.github.com>
2022-03-05 21:36:09 +0300

    Add files via upload

---
+++ Server.py
@@ -0,0 +1,18 @@
+import socket
+
+sock = socket.socket()
+sock.bind(('', 9090))
+
+sock.listen(1)
+
+print('waiting for connection...')
+conn, addr = sock.accept()
+
+print('connected: ', addr)
+
+data = conn.recv(1024)
+uData = data.decode("utf-8")
+print("Client > " + uData)
+conn.send(uData.encode("utf-8"))
+
+conn.close() 
